56f0621d90b47d98327f43fbc8ff82608ba8cedd
Your Name <your.email@example.com>
2025-05-19 05:20:46 +0700

    hihi

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,10 @@ app.use(
 )
 
 
-const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server Started on PORT ${PORT}`);
